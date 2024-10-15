89730be521d2c9f1c22893f2d97add101a2c03aa
jayanth2341 <yjayanth769@gmail.com>
2024-10-15 11:18:23 +0530

    Update reverse order.py

--- reverse order.py
+++ reverse order.py
@@ -1,7 +1,8 @@
 """
 Write a program to print the given list in reverse order.
 Sample Input:
-10 20 30 40 50
-Sample Output:
-50 40 30 20 10 
+10 20 30 40 50 
 """
+input_list = input("Enter the elements separated by space: ").split()
+reversed_list = input_list[::-1]
+print("Reversed list:", ' '.join(reversed_list))
