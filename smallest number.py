07e9eeffef0c374efe398dda4368eee46fbca2d2
jayanth2341 <yjayanth769@gmail.com>
2024-10-15 11:19:27 +0530

    Update smallest number.py

--- smallest number.py
+++ smallest number.py
@@ -12,6 +12,10 @@ Sample Input:
 3
 6
 5
-Sample Output:
-1
 """
+n = int(input("Enter the size of the list: "))elements = []
+for _ in range(n):
+    element = int(input("Enter an element: "))
+    elements.append(element)
+smallest_element = min(elements)
+print("The smallest element is:", smallest_element)
