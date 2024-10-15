a4dba978f6984d08a8c3c84d1a890bb3f63ed086
jayanth2341 <yjayanth769@gmail.com>
2024-10-15 11:20:32 +0530

    Update sort the given list.py

--- sort the given list.py
+++ sort the given list.py
@@ -2,6 +2,8 @@
 Write a program to sort the given list and print it.
 Sample Input:
 30 20 10 50 40
-Sample Output:
-10 20 30 40 50 
 """
+input_list = input("Enter the elements separated by space: ").split()
+number_list = [int(num) for num in input_list]
+sorted_list = sorted(number_list)
+print("Sorted list:", ' '.join(map(str, sorted_list)))
